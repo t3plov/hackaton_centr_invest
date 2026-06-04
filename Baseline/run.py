a6efff1f99d34a68560971f15d337712c51d0119
Ardrey Teplov <tepl.prod888@gmail.com>
2026-06-04 20:46:44 +0300

    lgbm_v5

--- Baseline/run.py
+++ Baseline/run.py
@@ -1,18 +1,14 @@
 import pandas as pd
-import numpy as np
+import sklearn
+print("Sklearn version in run:", sklearn.__version__)
 import joblib
 from pathlib import Path
 import argparse
 
-PRODUCTS = [
-    "credit_card", "mortgage", "deposit", "investment", "insurance",
-    "p2p_transfer", "cashback", "premium_account", "business_loan"
-]
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input-path', type=str, required=True, help='test data path')
-    parser.add_argument('--output-path', type=str, required=True, help='output file')
+    parser.add_argument('--input-path', type=str, required=True)
+    parser.add_argument('--output-path', type=str, required=True)
     args = parser.parse_args()
 
     test_data = pd.read_csv(args.input_path)
@@ -20,22 +16,18 @@ if __name__ == "__main__":
     pack = joblib.load(Path("baseline_model.joblib"))
     scaler = pack["scaler"]
     feat_cols = pack["feature_columns"]
-    models = pack["models"]
+    chain_model = pack["chain_model"]
 
     X = test_data[feat_cols].copy()
 
     for col in ['has_child', 'is_salary_client']:
-        if col in X.columns:
-            X[col] = X[col].astype(int)
+        if col in X.columns: X[col] = X[col].astype(int)
+    if 'income_bucket' in X.columns: X['income_bucket'] = X['income_bucket'].astype(int)
 
-    cols_to_scale = ['age', 'tenure_months', 'tx_count_30d', 'avg_tx_amount']
+    cols_to_scale = ['age', 'tenure_months', 'tx_count_30d', 'avg_tx_amount', 'digital_activity_score']
     X[cols_to_scale] = scaler.transform(X[cols_to_scale])
 
-    X_values = X.values
-
-    y_proba = np.column_stack([
-        models[p].predict_proba(X_values)[:, 1] for p in PRODUCTS
-    ])
+    y_proba = chain_model.predict_proba(X)
 
     result = pd.DataFrame(y_proba)
     result.to_csv(args.output_path, header=False, index=False)
--- Baseline/train.py
+++ Baseline/train.py
@@ -1,26 +1,29 @@
 import pandas as pd
 import numpy as np
 from pathlib import Path
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression
 import joblib
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import roc_auc_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.multioutput import ClassifierChain
+from lightgbm import LGBMClassifier
 
 PRODUCTS = [
     "credit_card", "mortgage", "deposit", "investment", "insurance",
     "p2p_transfer", "cashback", "premium_account", "business_loan"
 ]
 
-BEST_PARAMS = {
-    "credit_card": {'C': 0.1, 'penalty': 'l1', 'solver': 'saga', 'max_iter': 300, 'l1_ratio': 0.5},
-    "mortgage": {'C': 0.1, 'penalty': 'l1', 'solver': 'saga', 'max_iter': 300, 'l1_ratio': 0.5},
-    "deposit": {'C': 0.1, 'penalty': 'l1', 'solver': 'saga', 'max_iter': 300, 'l1_ratio': 0.5},
-    "investment": {'C': 0.1, 'penalty': 'l1', 'solver': 'saga', 'max_iter': 300, 'l1_ratio': 0.5},
-    "insurance": {'C': 0.1, 'penalty': 'l1', 'solver': 'saga', 'max_iter': 300, 'l1_ratio': 0.5},
-    "p2p_transfer": {'C': 1.0, 'penalty': 'l1', 'solver': 'saga', 'max_iter': 300, 'l1_ratio': 0.5},
-    "cashback": {'C': 0.1, 'penalty': None, 'solver': 'lbfgs', 'max_iter': 300, 'l1_ratio': 0.5},
-    "premium_account": {'C': 0.1, 'penalty': 'l1', 'solver': 'saga', 'max_iter': 300, 'l1_ratio': 0.5},
-    "business_loan": {'C': 0.1, 'penalty': None, 'solver': 'sag', 'max_iter': 300, 'l1_ratio': 0.5}
-}
+# BEST_PARAMS = {
+#     "credit_card": {'C': 0.1, 'penalty': 'l1', 'solver': 'saga', 'max_iter': 300, 'l1_ratio': 0.5},
+#     "mortgage": {'C': 0.1, 'penalty': 'l1', 'solver': 'saga', 'max_iter': 300, 'l1_ratio': 0.5},
+#     "deposit": {'C': 0.1, 'penalty': 'l1', 'solver': 'saga', 'max_iter': 300, 'l1_ratio': 0.5},
+#     "investment": {'C': 0.1, 'penalty': 'l1', 'solver': 'saga', 'max_iter': 300, 'l1_ratio': 0.5},
+#     "insurance": {'C': 0.1, 'penalty': 'l1', 'solver': 'saga', 'max_iter': 300, 'l1_ratio': 0.5},
+#     "p2p_transfer": {'C': 1.0, 'penalty': 'l1', 'solver': 'saga', 'max_iter': 300, 'l1_ratio': 0.5},
+#     "cashback": {'C': 0.1, 'penalty': None, 'solver': 'lbfgs', 'max_iter': 300, 'l1_ratio': 0.5},
+#     "premium_account": {'C': 0.1, 'penalty': 'l1', 'solver': 'saga', 'max_iter': 300, 'l1_ratio': 0.5},
+#     "business_loan": {'C': 0.1, 'penalty': None, 'solver': 'sag', 'max_iter': 300, 'l1_ratio': 0.5}
+# }
 
 
 def load_and_prepare_data():
@@ -31,39 +34,68 @@ def load_and_prepare_data():
     feature_cols = [c for c in df.columns if c not in ["user_id"] + target_cols]
 
     X = df[feature_cols].copy()
-    y = df[target_cols].values
+    y = df[target_cols].copy()
 
     for col in ['has_child', 'is_salary_client']:
         if col in X.columns:
             X[col] = X[col].astype(int)
 
+    columns_to_scaled = ['age', 'tenure_months', 'tx_count_30d', 'avg_tx_amount', 'digital_activity_score']
     scaler = StandardScaler()
-    X[['age', 'tenure_months', 'tx_count_30d', 'avg_tx_amount']] = scaler.fit_transform(X[['age', 'tenure_months', 'tx_count_30d', 'avg_tx_amount']])
+    X[columns_to_scaled] = scaler.fit_transform(X[columns_to_scaled])
 
-    return X, y, scaler, feature_cols
+    return X, y, scaler, feature_cols, target_cols
 
 
-def train_models(X, y):
-    models = {}
+def train_models(X, y, target_cols):
+    chain_order_names = [
+        'product_credit_card',
+        'product_cashback',
+        'product_p2p_transfer',
+        'product_deposit',
+        'product_investment',
+        'product_insurance',
+        'product_mortgage',
+        'product_business_loan',
+        'product_premium_account'
+    ]
+    order_indices = [target_cols.index(name) for name in chain_order_names]
 
-    for idx, prod in enumerate(PRODUCTS):
-        lr = LogisticRegression(**BEST_PARAMS[prod], random_state=42)
-        lr.fit(X, y[:, idx])
+    lgbm = LGBMClassifier(
+        n_estimators=100,
+        learning_rate=0.05,
+        num_leaves=10,
+        max_depth=2,
+        lambda_l1=0.001,
+        is_unbalance=True,
+        eval_metric='auc',
+        random_state=42,
+        verbose=-1,
+        n_jobs=-1
+    )
 
-        models[prod] = lr
+    chain_model = ClassifierChain(
+        lgbm,
+        order=order_indices,
+        random_state=42,
+        chain_method='predict_proba'
+    )
+    chain_model.fit(X, y)
 
-    return models
+    return chain_model
 
 
 if __name__ == "__main__":
-    X, y, scaler, feat_cols = load_and_prepare_data()
+    import sklearn
+    print("Sklearn version in train:", sklearn.__version__)
+    X, y, scaler, feat_cols, target_cols = load_and_prepare_data()
 
-    models = train_models(X, y)
+    chain_model = train_models(X, y, target_cols)
 
     model_pack = {
         "scaler": scaler,
         "feature_columns": feat_cols,
-        "models": models,
+        "chain_model": chain_model,
     }
 
     output_path = Path("./baseline_model.joblib")
