124b94b49c3f3884314a0e28a56aa29f221a12a0
Ardrey Teplov <tepl.prod888@gmail.com>
2026-06-05 16:12:39 +0300

    CatBoost:1.0

--- Baseline/run.py
+++ Baseline/run.py
@@ -1,9 +1,40 @@
 import pandas as pd
-import sklearn
-print("Sklearn version in run:", sklearn.__version__)
+import numpy as np
 import joblib
 from pathlib import Path
 import argparse
+
+def build_features(df):
+    df["tx_per_tenure"] = df["tx_count_30d"] / (df["tenure_months"] + 1)
+    df["avg_monthly_spend"] = df["tx_count_30d"] * df["avg_tx_amount"]
+    df["spend_per_tenure"] = df["avg_monthly_spend"] / (df["tenure_months"] + 1)
+    df["digital_intensity"] = df["digital_activity_score"] * df["tx_per_tenure"]
+    df["spend_per_income"] = df["avg_monthly_spend"] / (df["income_bucket"] + 1)
+    df["activity_per_income"] = df["tx_per_tenure"] / (df["income_bucket"] + 1)
+    df["log_tx"] = np.log1p(df["tx_count_30d"])
+    df["log_income"] = np.log1p(df["income_bucket"])
+    df["behavior_index"] = df["log_tx"] * df["digital_activity_score"]
+    df["log_avg_tx_amount"] = np.log1p(df["avg_tx_amount"])
+    df['age_bin'] = pd.cut(
+        df['age'],
+        bins=[18, 25, 35, 45, 55, 70],
+        labels=[
+            '18_25',
+            '26_35',
+            '36_45',
+            '46_55',
+            '56_70'
+        ],
+        include_lowest=True
+    )
+    age_dummies = pd.get_dummies(
+        df['age_bin'],
+        prefix='age',
+        dtype=int
+    )
+    df = pd.concat([df, age_dummies], axis=1)
+    df = df.drop(columns=['age', 'age_bin'])
+    return df
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -12,22 +43,19 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     test_data = pd.read_csv(args.input_path)
+    test_data = build_features(test_data)
 
     pack = joblib.load(Path("baseline_model.joblib"))
-    scaler = pack["scaler"]
     feat_cols = pack["feature_columns"]
-    chain_model = pack["chain_model"]
+    models = pack["model"]
 
     X = test_data[feat_cols].copy()
 
-    for col in ['has_child', 'is_salary_client']:
-        if col in X.columns: X[col] = X[col].astype(int)
-    if 'income_bucket' in X.columns: X['income_bucket'] = X['income_bucket'].astype(int)
+    proba_list = []
+    for col, model in models.items():
+        proba = model.predict_proba(X)[:, 1]
+        proba_list.append(proba)
 
-    cols_to_scale = ['age', 'tenure_months', 'tx_count_30d', 'avg_tx_amount', 'digital_activity_score']
-    X[cols_to_scale] = scaler.transform(X[cols_to_scale])
-
-    y_proba = chain_model.predict_proba(X)
-
-    result = pd.DataFrame(y_proba)
+    result = np.column_stack(proba_list)
+    result = pd.DataFrame(result)
     result.to_csv(args.output_path, header=False, index=False)
--- Baseline/train.py
+++ Baseline/train.py
@@ -2,33 +2,50 @@ import pandas as pd
 import numpy as np
 from pathlib import Path
 import joblib
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import train_test_split
+from catboost import CatBoostClassifier
 from sklearn.metrics import roc_auc_score
-from sklearn.preprocessing import StandardScaler
-from sklearn.multioutput import ClassifierChain
-from lightgbm import LGBMClassifier
 
 PRODUCTS = [
     "credit_card", "mortgage", "deposit", "investment", "insurance",
     "p2p_transfer", "cashback", "premium_account", "business_loan"
 ]
 
-# BEST_PARAMS = {
-#     "credit_card": {'C': 0.1, 'penalty': 'l1', 'solver': 'saga', 'max_iter': 300, 'l1_ratio': 0.5},
-#     "mortgage": {'C': 0.1, 'penalty': 'l1', 'solver': 'saga', 'max_iter': 300, 'l1_ratio': 0.5},
-#     "deposit": {'C': 0.1, 'penalty': 'l1', 'solver': 'saga', 'max_iter': 300, 'l1_ratio': 0.5},
-#     "investment": {'C': 0.1, 'penalty': 'l1', 'solver': 'saga', 'max_iter': 300, 'l1_ratio': 0.5},
-#     "insurance": {'C': 0.1, 'penalty': 'l1', 'solver': 'saga', 'max_iter': 300, 'l1_ratio': 0.5},
-#     "p2p_transfer": {'C': 1.0, 'penalty': 'l1', 'solver': 'saga', 'max_iter': 300, 'l1_ratio': 0.5},
-#     "cashback": {'C': 0.1, 'penalty': None, 'solver': 'lbfgs', 'max_iter': 300, 'l1_ratio': 0.5},
-#     "premium_account": {'C': 0.1, 'penalty': 'l1', 'solver': 'saga', 'max_iter': 300, 'l1_ratio': 0.5},
-#     "business_loan": {'C': 0.1, 'penalty': None, 'solver': 'sag', 'max_iter': 300, 'l1_ratio': 0.5}
-# }
-
 
 def load_and_prepare_data():
     train_path = Path("./train_data.csv")
     df = pd.read_csv(train_path)
+
+    df["tx_per_tenure"] = df["tx_count_30d"] / (df["tenure_months"] + 1)
+    df["avg_monthly_spend"] = df["tx_count_30d"] * df["avg_tx_amount"]
+    df["spend_per_tenure"] = df["avg_monthly_spend"] / (df["tenure_months"] + 1)
+    df["digital_intensity"] = df["digital_activity_score"] * df["tx_per_tenure"]
+    df["spend_per_income"] = df["avg_monthly_spend"] / (df["income_bucket"] + 1)
+    df["activity_per_income"] = df["tx_per_tenure"] / (df["income_bucket"] + 1)
+    df["log_tx"] = np.log1p(df["tx_count_30d"])
+    df["log_income"] = np.log1p(df["income_bucket"])
+    df["behavior_index"] = df["log_tx"] * df["digital_activity_score"]
+    df["log_avg_tx_amount"] = np.log1p(df["avg_tx_amount"])
+
+    df['age_bin'] = pd.cut(
+        df['age'],
+        bins=[18, 25, 35, 45, 55, 70],
+        labels=[
+            '18_25',
+            '26_35',
+            '36_45',
+            '46_55',
+            '56_70'
+        ],
+        include_lowest=True
+    )
+    age_dummies = pd.get_dummies(
+        df['age_bin'],
+        prefix='age',
+        dtype=int
+    )
+    df = pd.concat([df, age_dummies], axis=1)
+    df = df.drop(columns=['age', 'age_bin'])
 
     target_cols = [f"product_{p}" for p in PRODUCTS]
     feature_cols = [c for c in df.columns if c not in ["user_id"] + target_cols]
@@ -36,66 +53,68 @@ def load_and_prepare_data():
     X = df[feature_cols].copy()
     y = df[target_cols].copy()
 
-    for col in ['has_child', 'is_salary_client']:
-        if col in X.columns:
-            X[col] = X[col].astype(int)
-
-    columns_to_scaled = ['age', 'tenure_months', 'tx_count_30d', 'avg_tx_amount', 'digital_activity_score']
-    scaler = StandardScaler()
-    X[columns_to_scaled] = scaler.fit_transform(X[columns_to_scaled])
-
-    return X, y, scaler, feature_cols, target_cols
+    return X, y, feature_cols, target_cols
 
 
 def train_models(X, y, target_cols):
-    chain_order_names = [
-        'product_credit_card',
-        'product_cashback',
-        'product_p2p_transfer',
-        'product_deposit',
-        'product_investment',
-        'product_insurance',
-        'product_mortgage',
-        'product_business_loan',
-        'product_premium_account'
-    ]
-    order_indices = [target_cols.index(name) for name in chain_order_names]
-
-    lgbm = LGBMClassifier(
-        n_estimators=100,
-        learning_rate=0.05,
-        num_leaves=10,
-        max_depth=2,
-        lambda_l1=0.001,
-        is_unbalance=True,
-        eval_metric='auc',
-        random_state=42,
-        verbose=-1,
-        n_jobs=-1
+    X_train, X_val, y_train, y_val = train_test_split(
+        X, y,
+        test_size=0.15,
+        random_state=42
     )
 
-    chain_model = ClassifierChain(
-        lgbm,
-        order=order_indices,
-        random_state=42,
-        chain_method='predict_proba'
-    )
-    chain_model.fit(X, y)
+    models = {}
 
-    return chain_model
+    for col in target_cols:
+        model = CatBoostClassifier(
+            iterations=4000,  # больше итераций + early stopping
+            learning_rate=0.02,  # ниже = стабильнее обобщение
+
+            depth=5,  # sweet spot для таблички
+            l2_leaf_reg=8,  # сильнее регуляризация (важно!)
+            random_strength=2,  # шум в сплитах (борьба с overfit)
+
+            bagging_temperature=0.8,  # стохастичность выборки
+            subsample=0.8,  # row sampling
+
+            loss_function="Logloss",
+            eval_metric="AUC",
+
+            leaf_estimation_iterations=5,
+            leaf_estimation_method="Gradient",
+
+            grow_policy="Lossguide",  # часто лучше на табличке
+            min_data_in_leaf=50,  # ключевая защита от переобучения
+
+            od_type="Iter",
+            od_wait=200,
+
+            verbose=200,
+            random_seed=42
+        )
+
+        model.fit(
+            X_train, y_train[col],
+            eval_set=(X_val, y_val[col]),
+            use_best_model=True,
+            verbose=200
+        )
+
+        models[col] = model
+
+    return models
 
 
 if __name__ == "__main__":
     import sklearn
     print("Sklearn version in train:", sklearn.__version__)
-    X, y, scaler, feat_cols, target_cols = load_and_prepare_data()
+    X, y, feat_cols, target_cols = load_and_prepare_data()
 
-    chain_model = train_models(X, y, target_cols)
+    models = train_models(X, y, target_cols)
 
     model_pack = {
-        "scaler": scaler,
         "feature_columns": feat_cols,
-        "chain_model": chain_model,
+        "model": models,
     }
 
     output_path = Path("./baseline_model.joblib")
